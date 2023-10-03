5dfdf3c0636df843b0ce2e54ed84cca59d8ad104
jaimeajl <jaimeajl@hotmail.com>
2023-10-02 19:40:57 -0500

    Descripción de los cambios

--- FastApi2.py
+++ FastApi2.py
@@ -15,24 +15,22 @@ user_game_matrix = pd.pivot_table(merged_df, values='playtime_forever', index='s
 cosine_sim = cosine_similarity(user_game_matrix.T)
 
 # Función para obtener recomendaciones basadas en un juego de entrada
-def get_recommendations(input_game, cosine_sim):
+def get_recommendations(input_game, cosine_sim, user_game_matrix, dfsteam_games_cleaned):
     # Encuentra el índice del juego de entrada
     idx = user_game_matrix.columns.get_loc(input_game)
-    # Obtén las puntuaciones de similitud para ese juego
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    # Ordena los juegos según la similitud
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    # Obtén los 10 juegos más similares (excluyendo el juego de entrada)
-    similar_games = sim_scores[1:6]
+    # Obtén las puntuaciones de similitud para ese juego y obtén los 5 juegos más similares
+    similar_games = list(enumerate(cosine_sim[idx]))
+    similar_games = sorted(similar_games, key=lambda x: x[1], reverse=True)[:5]
     # Obtén los IDs de los juegos recomendados
-    recommended_games = [user_game_matrix.columns[i[0]] for i in similar_games]
+    recommended_games = [user_game_matrix.columns[i] for i, _ in similar_games]
     # Mapea los IDs de juego a los nombres de juego
     recommended_game_names = dfsteam_games_cleaned[dfsteam_games_cleaned['id'].isin(recommended_games)]['title'].tolist()
     return recommended_game_names
 
 # Ejemplo de recomendaciones para un juego de entrada
 input_game_id = 273350
-recommendations = get_recommendations(input_game_id, cosine_sim)
+recommendations = get_recommendations(input_game_id, cosine_sim, user_game_matrix, dfsteam_games_cleaned)
 print(f"Juegos recomendados para el juego con ID {input_game_id}:")
 print(recommendations)
+
 
